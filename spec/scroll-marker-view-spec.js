96a7265e10d41ea956161edcb3a25ff2e7388e90
Nicu <postal2600@gmail.com>
2017-05-19 19:48:20 +0300

    init

---
+++ spec/scroll-marker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ScrollMarkerView from '../lib/scroll-marker-view';
+
+describe('ScrollMarkerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
