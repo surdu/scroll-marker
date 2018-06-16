41edf9f9bfc2b5958b936037fb2aa5259170933f
surdu <postal2600@gmail.com>
2018-06-16 04:06:38 +0300

    Removed dummy tests

--- spec/scroll-marker-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import ScrollMarkerView from '../lib/scroll-marker-view';
-
-describe('ScrollMarkerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
